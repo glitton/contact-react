25c782bcf6fa32f3c0c0b154d5cbb3be0953ac6a
Generosa Litton <glitton@newrelic.com>
2019-07-16 17:38:07 -0700

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: "swordfish",
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector('input[type="password"]').value;
+    const auth = password === this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    return (
+      <div id="authorization">
+        <h1>Contact</h1>
+        <ul>
+          <li>client@example.com</li>
+          <li>555.555.5555</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
