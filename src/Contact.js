01dd0b2bbd9b5a65e3231e84485b04bb11c1d75d
Generosa Litton <glitton@newrelic.com>
2019-07-16 17:48:34 -0700

    added ternary operators

--- src/Contact.js
+++ src/Contact.js
@@ -1,31 +1,36 @@
 import React from "react";
 
 class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      password: "swordfish",
-      authorized: false
-    };
-    this.authorize = this.authorize.bind(this);
-  }
+  state = {
+    password: "lunatic",
+    authorized: false
+  };
 
-  authorize(e) {
+  authorize = e => {
     const password = e.target.querySelector('input[type="password"]').value;
     const auth = password === this.state.password;
     this.setState({
       authorized: auth
     });
-  }
+  };
 
   render() {
+    const login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" placeholder="Password" />
+        <input type="submit" placeholder="Password" />
+      </form>
+    );
+    const contactInfo = (
+      <ul>
+        <li>client@example.com</li>
+        <li>555.555.5555</li>
+      </ul>
+    );
     return (
       <div id="authorization">
-        <h1>Contact</h1>
-        <ul>
-          <li>client@example.com</li>
-          <li>555.555.5555</li>
-        </ul>
+        <h1>{this.state.authorized ? "Contact" : "Enter the Password"}</h1>
+        {this.state.authorized ? contactInfo : login}
       </div>
     );
   }
